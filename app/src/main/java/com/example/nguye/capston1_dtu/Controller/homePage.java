08b12f292c7c860d883cc93fd648cb46558001fa
kalimess16 <kalimess1997@gmail.com>
2019-04-05 19:23:25 +0700

    image-bai-hoc,screnn, show test de1,2,3,4,5

---
+++ app/src/main/java/com/example/nguye/capston1_dtu/Controller/homePage.java
@@ -0,0 +1,28 @@
+package com.example.nguye.capston1_dtu.Controller;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.nguye.capston1_dtu.R;
+
+public class homePage extends Fragment {
+    Button button;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home_page,null);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
